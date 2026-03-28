16a1b4f5ffeb1d31c1805fa82e7f02fb70649796
Malekibdah15-ai <mibdah15@gmail.com>
2026-03-28 20:18:23 +0300

    feat: add WebSocket support and enhance chat functionality with message handling

--- mobile/components/ChatItem.tsx
+++ mobile/components/ChatItem.tsx
@@ -2,19 +2,16 @@ import { Chat } from "@/types";
 import { Image } from "expo-image";
 import { View, Text, Pressable } from "react-native";
 import { formatDistanceToNow } from "date-fns";
-// import { useSocketStore } from "@/lib/socket";
+import { useSocketStore } from "@/lib/socket";
 
 const ChatItem = ({ chat, onPress }: { chat: Chat; onPress: () => void }) => {
   const participant = chat.participant;
 
-//   const { onlineUsers, typingUsers, unreadChats } = useSocketStore();
+  const { onlineUsers, typingUsers, unreadChats } = useSocketStore();
 
-  const isOnline = true
-// onlineUsers.has(participant._id);
-  const isTyping = false
-//   typingUsers.get(chat._id) === participant._id;
-  const hasUnread = false
-//   unreadChats.has(chat._id);
+  const isOnline = onlineUsers.has(participant._id);
+  const isTyping = typingUsers.get(chat._id) === participant._id;
+  const hasUnread = unreadChats.has(chat._id);
 
   return (
     <Pressable className="flex-row items-center py-3 active:opacity-70" onPress={onPress}>
